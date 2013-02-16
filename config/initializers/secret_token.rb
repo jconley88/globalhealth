2f45d5cd3de78468a885045c2269cc6b1f93d46e
Jon Conley <conley.jj@gmail.com>
2013-02-16 13:28:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Globalhealth::Application.config.secret_token = 'dc34ae3461dcb73ab02df7624385752e6b5a8dfd8037d4f0213b04e3f3508e537e99414dcd7acb24afb19b85633a67be3023517082ff36b8160ca497d22c211a'
